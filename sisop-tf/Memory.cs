cfae19e1e0de144b3398378dcbd7ec494bb08439
lucas.cunha2104 <lucas.cunha2104@336d0950-bd97-7aad-9c28-c86f7ce6f202>
2014-03-19 01:20:43 +0000

    Ajuste: divisão do código procedural e métodos base.

---
+++ sisop-tf/Memory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace sisop_tf
+{
+	public class Memory
+	{
+		private string[] memory;
+		private int key;
+
+		public Memory()
+		{
+			memory = new string[8 * 1024];
+			key = 0;
+		}
+
+		public void Add(string value)
+		{
+			memory[key] = value;
+			key++;
+		}
+
+		public string Get(int key)
+		{
+			return memory[key];
+		}
+
+		public void Set(int key, string value)
+		{
+			memory[key] = value;
+		}
+
+		public int GetIndex()
+		{
+			return key;
+		}
+
+		// TODO: Rever funcionalidade
+		public bool HasNext(int key)
+		{
+			return !((string.IsNullOrEmpty(memory[key]) && string.IsNullOrEmpty(memory[key + 1])) || key >= memory.Length);
+		}
+	}
+}
