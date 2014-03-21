43f1bda5454e98e62a1b1e9333fe6a0072d50034
lucas.cunha2104 <lucas.cunha2104@336d0950-bd97-7aad-9c28-c86f7ce6f202>
2014-03-21 01:54:11 +0000

    Ajustes: - Implementação da classe Process. - Ajuste no carregamento do arquivo ASM para utilizar a classe Process.

--- trunk/sisop-tf/Classes/Process.cs
+++ trunk/sisop-tf/Classes/Process.cs
@@ -1,13 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace sisop_tf
 {
 	public class Process
 	{
+		public int BeginData { get; private set; }
+		public int BeginCode { get; private set; }
+		public int EndCode { get; private set; }
 
+		public int Pc { get; set; }
+		public int Ac { get; set; }
+
+		public Process(int beginData, int beginCode, int endCode)
+		{
+			BeginData = beginData;
+			BeginCode = beginCode;
+			EndCode = endCode;
+
+			Pc = beginCode;
+		}
 	}
 }
