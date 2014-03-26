4dbe86958adfb1e6450ca4d5864cc0054882bc80
fabiorochadacosta@gmail.com <fabiorochadacosta@gmail.com@336d0950-bd97-7aad-9c28-c86f7ce6f202>
2014-03-26 01:32:25 +0000

    Adicao de enum e classes

---
+++ sisop-tf/Enums/Priority.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisop_tf.Enums
+{
+    public enum Priority
+    {
+        Baixa = 0,
+        Medio = 1,
+        Alta = 2
+    }
+}
