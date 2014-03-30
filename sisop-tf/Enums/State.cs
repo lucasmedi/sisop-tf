6ef5e55209a414be395e8a1e1f2143a5449a0b12
lucas.cunha2104 <lucas.cunha2104@336d0950-bd97-7aad-9c28-c86f7ce6f202>
2014-03-30 21:54:05 +0000

    Adicionado enum State.

--- sisop-tf/Enums/State.cs
+++ sisop-tf/Enums/State.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace sisop_tf.Enums
+﻿
+namespace sisop_tf
 {
-    public enum State
-    {
-        New = 0,
-        Ready = 1,
-        ReadySuspend = 2,
-        Blocked = 3,
-        BlockedSuspend = 4,
-        Running = 5,
-        Exit = 6
-    }
+	public enum State
+	{
+		New = 0,
+		Ready = 1,
+		ReadySuspend = 2,
+		Blocked = 3,
+		BlockedSuspend = 4,
+		Running = 5,
+		Exit = 6
+	}
 }
