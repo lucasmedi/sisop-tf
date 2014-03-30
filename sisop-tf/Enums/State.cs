7ba7860c0c19d11dd17fb20d4529dc9af0477d4d
fabiorochadacosta@gmail.com <fabiorochadacosta@gmail.com@336d0950-bd97-7aad-9c28-c86f7ce6f202>
2014-03-30 21:06:41 +0000

    Adicao de states

---
+++ sisop-tf/Enums/State.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisop_tf.Enums
+{
+    public enum State
+    {
+        New = 0,
+        Blocked = 1,
+        Suspended = 2,
+        Running = 3,
+        Exit = 4
+    }
+}
