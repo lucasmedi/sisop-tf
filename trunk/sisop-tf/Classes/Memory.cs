ac188f2574de8ac9c75387768bbf5694eb2ee1dd
lucas.cunha2104 <lucas.cunha2104@336d0950-bd97-7aad-9c28-c86f7ce6f202>
2014-03-30 21:08:37 +0000

    Método para verificar se há espaço disponível na memória.

--- trunk/sisop-tf/Classes/Memory.cs
+++ trunk/sisop-tf/Classes/Memory.cs
@@ -18,19 +18,50 @@ namespace sisop_tf
 			key++;
 		}
 
-		public string Get(int key)
-		{
-			return memory[key];
-		}
-
 		public void Set(int key, string value)
 		{
 			memory[key] = value;
 		}
 
+		public string Get(int key)
+		{
+			return memory[key];
+		}
+
 		public int GetIndex()
 		{
 			return key;
+		}
+
+		public bool HasSpace(int size, out int position)
+		{
+			var startPosition = -1;
+			var freeSize = 0;
+			for (int i = 0; i < memory.Length; i++)
+			{
+				if (string.IsNullOrEmpty(memory[i]))
+				{
+					freeSize++;
+					if (freeSize == 1)
+					{
+						startPosition = i;
+					}
+					
+					if (freeSize == size)
+					{
+						position = startPosition;
+						return true;
+					}
+				}
+				else
+				{
+					freeSize = 0;
+					startPosition = -1;
+				}
+			}
+
+			position = -1;
+			return false;
 		}
 
 		/// <summary>
